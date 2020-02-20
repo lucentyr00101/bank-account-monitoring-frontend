1957b35a2ef88dfb2d8874426b7c5f1cf6af7349
Ranz Sarcos <rsarcos@yondu.com>
2020-02-20 14:40:23 +0800

    app bar and drawer converted to components

---
+++ src/components/nav-drawer.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  v-navigation-drawer(app clipped v-model="drawer" color="secondary lighten-2" expand-on-hover :mini-variant="true")
+    v-list(dense)
+      v-list-item(@click="")
+        v-list-item-icon
+          v-icon mdi-home
+        v-list-item-content
+          v-list-item-title Home
+      v-list-group(active-class="menu-active-list" v-for="item in items" :key="item.title" v-model="item.active" :prepend-icon="item.action" no-action color="white")
+        template(v-slot:activator)
+          v-list-item-content
+            v-list-item-title(v-text="item.title")
+        v-list-item(v-for="subItem in item.items" :key="subItem.title" @click="")
+          v-list-item-content
+            v-list-item-title(v-text="subItem.title")
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        {
+          action: 'mdi-settings',
+          title: 'Settings',
+          items: [
+            { title: 'Profile Settings' }
+          ],
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .menu-active-list
+    color #f7a7a6 !important
+</style>
