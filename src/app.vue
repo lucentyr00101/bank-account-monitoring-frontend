5dfa5eb328a864f52514f2029683337f0cb196d8
Ranz Sarcos <rsarcos@yondu.com>
2020-02-20 13:33:13 +0800

    added navbar and footer design

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(app v-if="this.$auth.isAuthenticated()")
-    v-app-bar(app)
+    v-navigation-drawer(app v-if="this.$auth.isAuthenticated()" clipped v-model="drawer" color="primary darken-2")
+    v-app-bar(app color="primary" clipped-left)
+      v-app-bar-nav-icon(@click="drawer = !drawer")
+      v-toolbar-title Title
     v-content
       v-container.h-100(fluid)
         router-view
-    v-footer(app)
+    v-footer(app color='primary' padless)
+      v-col.text-center.overline(cols="12") &copy; {{ new Date().getFullYear() }}
+
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
 
