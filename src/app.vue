76b38f037836e473f187b4478f3538b92a772f0c
Ranz Sarcos <rsarcos@yondu.com>
2020-02-20 13:59:17 +0800

    added nav drawer contents and redesign

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,20 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(app v-if="this.$auth.isAuthenticated()" clipped v-model="drawer" color="primary darken-2")
+    v-navigation-drawer(app v-if="this.$auth.isAuthenticated()" clipped v-model="drawer" color="secondary lighten-2" expand-on-hover :mini-variant="true")
+      v-list(dense)
+        v-list-item(@click="")
+          v-list-item-icon
+            v-icon mdi-home
+          v-list-item-content
+            v-list-item-title Home
+        v-list-group(active-class="menu-active-list" v-for="item in items" :key="item.title" v-model="item.active" :prepend-icon="item.action" no-action color="white")
+          template(v-slot:activator)
+            v-list-item-content
+              v-list-item-title(v-text="item.title")
+          v-list-item(v-for="subItem in item.items" :key="subItem.title" @click="")
+            v-list-item-content
+              v-list-item-title(v-text="subItem.title")
     v-app-bar(app color="primary" clipped-left)
-      v-app-bar-nav-icon(@click="drawer = !drawer")
       v-toolbar-title Title
     v-content
       v-container.h-100(fluid)
@@ -16,7 +28,16 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: true,
+      items: [
+        {
+          action: 'mdi-settings',
+          title: 'Settings',
+          items: [
+            { title: 'Profile Settings' }
+          ],
+        }
+      ]
     }
   }
 }
@@ -28,4 +49,6 @@ export default {
     padding: 0
     .h-100
       height 100%
+    .menu-active-list
+      color #f7a7a6 !important
 </style>
