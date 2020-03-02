8c6399d90272b2e689679e08a2cde062e050478e
Ranz Sarcos <rsarcos@yondu.com>
2020-03-02 13:42:43 +0800

    added momentjs and date field

--- src/views/funds/create.vue
+++ src/views/funds/create.vue
@@ -5,26 +5,33 @@
         v-card(shaped)
           v-card-text
             p.headline Add Funds
-            v-text-field(label="Amount" prepend-inner-icon="mdi-currency-php" :rules="amountRules")
-            .d-flex.justify-end.my-1
-              v-btn(type="submit" color="success" :disabled="!valid") Save
+            v-text-field(label="Amount" prepend-inner-icon="mdi-currency-php" :rules="[rules.number, rules.required]")
+            v-text-field(label="Date" prepend-inner-icon="mdi-calendar-month" :rules="[rules.required]" disabled :value="getDate")
+            v-btn.mt-5(type="submit" color="success" :disabled="!valid") Save
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {  
   data() {
     return {
       valid: true,
-      amountRules: [
-        v => !!v || 'Amount field is required!',
-        v => !isNaN(v) || 'Amount field must be a number!'
-      ]
+      rules: {
+        number: v => !isNaN(v) || 'Amount field must be a number!',
+        required: v => !!v || 'This field is required!'
+      }
     }
   },
   methods: {
     submitForm() {
       this.$refs.form.validate()
       console.log('form submitted')
+    }
+  },
+  computed: {
+    getDate() {
+      return moment().format('MMMM DD, YYYY')
     }
   }
 }
